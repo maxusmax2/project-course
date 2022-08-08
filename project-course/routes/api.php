1457c72303fcdff4613b30ac01704f846ea5a6d1
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-08-08 15:48:06 +0300

    Добавление роутов под названия

--- project-course/routes/api.php
+++ project-course/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\CompareController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/catalog/sale-{buildType}/{id}',[DetailController::class,'getDetail']);
+
+Route::middleware('auth:api')->group(function(){
+
+    Route::post('/catalog/sale-{buildType}/{id}/append-favorite',[FavoriteController::class,'appendFavorite']);
+    Route::post('/catalog/sale-{buildType}/{id}/delete-favorite',[FavoriteController::class,'deleteFavorite']);
+    Route::get('/favorite',[FavoriteController::class,'getFavorite']);
+
+    Route::post('/catalog/sale-{buildType}/{id}/append-compare',[CompareController::class,'appendCompare']);
+    Route::post('/catalog/sale-{buildType}/{id}/delete-compare',[CompareController::class,'deleteCompare']);
+    Route::get('/compare',[CompareController::class,'getCompare']);
 });
