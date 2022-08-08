0464a92cf11f389f59c2e3ba4a695f290b20ab5d
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-08-08 15:43:37 +0300

    Добавление миграций

---
+++ project-course/database/migrations/2022_08_08_120420_create_cottages_images.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cottage_images', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('build_id');
+            $table->foreign('build_id')->references('id')->on('cottages')->onDelete('cascade');
+            $table->string('image_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cottages_images');
+    }
+};
