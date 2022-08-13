7c51cc697543f6026b2a0ae825761487681db983
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-08-13 19:04:39 +0300

    Добавлена задача в планировщик задач
    Команда каждый день в 00:00  активирует событие EndDayEvent

--- project-course/app/Console/Kernel.php
+++ project-course/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Events\EndDayEvent;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,6 +16,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function(){
+            EndDayEvent::dispatch();
+        })->daily();
         // $schedule->command('inspire')->hourly();
     }
 
