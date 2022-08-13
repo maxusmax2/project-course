23a9410e6242044a32aa26e84c5045c30608a92f
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-08-13 15:40:35 +0300

    Добавлены в контроллеры трейты добавлена отдача  Url с помощью Storage и  логика получения определнного здания из БД
    Из-за того что были изменены названия таблиц  под роуты
    Получения данных из конкретной  таблицы происходит по параметру из роута BuildType и  с использованием DB фасада и его метода table

--- project-course/app/Http/Controllers/CompareController.php
+++ project-course/app/Http/Controllers/CompareController.php
@@ -8,67 +8,27 @@ use App\Models\Commercial;
 use App\Models\Cottage;
 use App\Models\Land;
 use App\Models\Room;
-
+use Illuminate\Support\Facades\DB;
 
 class CompareController extends Controller
 {
+    use BuildResponseTrait;
+
     public function getCompare(Request $request){
+
+        $Response = [];
+
         $builds = CompareBuild::
             where('user_token',$request->user()->token()->id)
             ->get();
-        $Response = [];
+
         foreach($builds as $build){
-        switch ($build->build_type){
-            case 'rooms':
-                $buildObject = Room::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('room_images','rooms.id','=','room_images.build_id')
-                    ->select('room_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
 
-            case 'apartments':
-
-                $buildObject = Apartment::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('apartment_images','apartments.id','=','apartment_images.build_id')
-                    ->select('apartment_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'land':
-
-                $buildObject = Land::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('land_images','lands.id','=','land_images.build_id')
-                    ->select('land_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'cottage':
-
-                $buildObject = Cottage::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('cottage_images','cottages.id','=','cottage_images.build_id')
-                    ->select('cottage_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'commercial':
-
-                $buildObject = Commercial::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('commercial_images','commercials.id','=','commercial_images.build_id')
-                    ->select('commercial_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
+            $buildObject = DB::table($build->build_type)->find($build->build_id);
+            $buildImages = $this->getImageBuild(DB::table($build->build_type),$build->build_type);
+            $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
         }
-        }
+
         return $Response;
     }
 
@@ -88,9 +48,5 @@ class CompareController extends Controller
             ->where('build_id',$id)
             ->where('user_token',$request->user()->token()->id)
             ->delete();
-    }
-
-    private function packCompareBuild($buildInfo, $images){
-        return ['buildInfo'=>$buildInfo,'images'=>$images];
     }
 }
--- project-course/app/Http/Controllers/FavoriteController.php
+++ project-course/app/Http/Controllers/FavoriteController.php
@@ -9,67 +9,28 @@ use App\Models\Commercial;
 use App\Models\Cottage;
 use App\Models\Land;
 use App\Models\Room;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class FavoriteController extends Controller
 {
+    use BuildResponseTrait;
     public function getFavorite(Request $request){
+
+        $Response = [];
+
         $builds = FavoriteBuild::
             where('user_token',$request->user()->token()->id)
             ->get();
-        $Response = [];
+
         foreach($builds as $build){
-        switch ($build->build_type){
-            case 'rooms':
-                $buildObject = Room::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('room_images','rooms.id','=','room_images.build_id')
-                    ->select('room_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
 
-            case 'apartments':
-
-                $buildObject = Apartment::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('apartment_images','apartments.id','=','apartment_images.build_id')
-                    ->select('apartment_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'land':
-
-                $buildObject = Land::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('land_images','lands.id','=','land_images.build_id')
-                    ->select('land_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'cottage':
-
-                $buildObject = Cottage::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('cottage_images','cottages.id','=','cottage_images.build_id')
-                    ->select('cottage_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
-
-            case 'commercial':
-
-                $buildObject = Commercial::find($build->build_id);
-                $buildImages = $buildObject
-                    ->join('commercial_images','commercials.id','=','commercial_images.build_id')
-                    ->select('commercial_images.image_name')
-                    ->get();
-                    $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
-                break;
+            $buildObject = DB::table($build->build_type)->find($build->build_id);
+            $buildImages = $this->getImageBuild(DB::table($build->build_type),$build->build_type);
+            $Response[] = $this->packFavoriteBuild($buildObject,$buildImages);
         }
-        }
+
         return $Response;
     }
 
@@ -92,7 +53,5 @@ class FavoriteController extends Controller
             ->delete();
     }
 
-    private function packFavoriteBuild($buildInfo, $images){
-        return ['buildInfo'=>$buildInfo,'images'=>$images];
-    }
+
 }
